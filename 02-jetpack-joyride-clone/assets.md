1b0a4fee0e3b0d55210135f4df9a497592d1b67f
David Saltares <david.saltares@gmail.com>
2024-11-19 09:31:11 -0500

    feat: 02 - player running and flying

---
+++ 02-jetpack-joyride-clone/assets.md
@@ -0,0 +1,2 @@
+- [Kenney Platformer Art Deluxe](https://kenney.nl/assets/platformer-art-deluxe) by [Kenney](https://kenney.nl/) - [CC0 1.0](https://creativecommons.org/publicdomain/zero/1.0/)
+- SFX generated with [sfxr](https://sfxr.me/)
---
+++ 02-jetpack-joyride-clone/player.gd
@@ -0,0 +1,26 @@
+class_name Player
+extends CharacterBody2D
+
+@export var gravity := 700.0
+@export var boost_acceleration := -900.0
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	_update_movement(delta)
+	_update_animation()
+
+func _update_movement(delta: float) -> void:
+	var acceleration := gravity
+	if Input.is_action_pressed('boost'):
+		acceleration = boost_acceleration
+	
+	velocity.y += acceleration * delta
+	
+	move_and_slide()
+	
+func _update_animation() -> void:
+	if is_on_floor():
+		animated_sprite.play('run')
+	else:
+		animated_sprite.play('jump')

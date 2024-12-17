b4b808c056b4865362175ca52458b49d48969fb0
David Saltares <david.saltares@gmail.com>
2024-12-17 11:43:08 +0200

    feat: 03 - use 360x640 base resolution and scale to the maximum integer the monitor allows

--- 03-river-raid-clone/game_camera.gd
+++ 03-river-raid-clone/game_camera.gd
@@ -3,6 +3,18 @@ extends Camera2D
 
 @export var target : Player
 
+@onready var bottom: CollisionShape2D = $Bounds/Bottom
+@onready var top: CollisionShape2D = $Bounds/Top
+@onready var left: CollisionShape2D = $Bounds/Left
+@onready var right: CollisionShape2D = $Bounds/Right
+
+func _ready() -> void:
+	var viewport_rect := get_viewport_rect()
+	bottom.position = Vector2(0.0, viewport_rect.size.y * 0.5 * zoom.y)
+	top.position = Vector2(0.0, -viewport_rect.size.y * 0.5 * zoom.y)
+	right.position = Vector2(viewport_rect.size.x * 0.5 * zoom.x, 0.0)
+	left.position = Vector2(-viewport_rect.size.x * 0.5 * zoom.x, 0.0)
+
 func _physics_process(delta: float) -> void:
 	if not target:
 		return
--- 03-river-raid-clone/main.gd
+++ 03-river-raid-clone/main.gd
@@ -1,7 +1,26 @@
 class_name Main
 extends Node2D
 
+func _ready() -> void:
+	_setup_window()
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed('restart'):
 		get_tree().call_group('transient', 'queue_free')
 		get_tree().reload_current_scene()
+
+func _setup_window() -> void:
+	var screen_size := DisplayServer.screen_get_size()
+	var viewport_size := get_viewport().get_visible_rect().size
+	var max_width_multiple := floorf(screen_size.x / viewport_size.x)
+	var max_height_multiple := floorf(screen_size.y / viewport_size.y)
+	var scale_multiplier := mini(max_height_multiple, max_width_multiple)
+	if max_width_multiple > max_height_multiple:
+		scale_multiplier = max_height_multiple - 1
+	var window_size := Vector2i(
+		viewport_size.x * scale_multiplier,
+		viewport_size.y * scale_multiplier
+	)
+	var centered_position := (screen_size - window_size) / 2
+	DisplayServer.window_set_size(window_size)
+	DisplayServer.window_set_position(centered_position)
